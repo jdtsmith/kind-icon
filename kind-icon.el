099aaf235215e29ee280fbb88b0c0e79f64eb61f
JD Smith <93749+jdtsmith@users.noreply.github.com>
2022-06-12 11:44:05 -0400

    Bump version

--- kind-icon.el
+++ kind-icon.el
@@ -5,7 +5,7 @@
 ;; Author: J.D. Smith <jdtsmith@gmail.com>
 ;; URL: https://github.com/jdtsmith/kind-icon
 ;; Package-Requires: ((emacs "27.1") svg-lib)
-;; Version: 0.1.5
+;; Version: 0.1.6
 ;; Keywords: completion
 
 ;; kind-icon is free software: you can redistribute it
